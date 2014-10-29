132bd34c62dae3a0899eb323afb3eb9637d44cbe
lizportnoy <lizport10@gmail.com>
2014-10-29 16:05:27 -0700

    Finished first part of data structures

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var makeQueue = function(){
   var someInstance = {};
+  var count = 0;
+
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,13 +9,59 @@ var makeQueue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[count] = value;
+    count += 1;
   };
 
   someInstance.dequeue = function(){
+    var firstOut = storage[0];
+    delete storage[0];
+    _.each(storage, function (val, key) {
+      storage[+key-1] = val;
+    });
+    delete storage[count];
+    if (count) {
+      count --;
+    }
+    return firstOut;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
 };
+
+/* var someInstance = {};
+  var count = 0;
+  var oldest = 0;
+  var newKey = 0;
+
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  // Implement the methods below
+
+  someInstance.enqueue = function(value){
+    storage[newKey] = value;
+    newKey ++;
+    count++;
+  };
+
+  someInstance.dequeue = function(){
+    if (count > 0) {
+      count--;
+    }
+    var firstOut = storage[oldest];
+    delete storage[oldest];
+    return firstOut;
+    oldest++;
+  };
+
+  someInstance.size = function(){
+    return count;
+  };
+
+  return someInstance; */
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,27 @@
 var makeStack = function(){
   var someInstance = {};
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;
+    count += 1;
   };
 
   someInstance.pop = function(){
+    if (count > 0) {
+      count -= 1;
+    }
+    var firstOut = storage[count];
+    delete storage[count];
+    return firstOut;
   };
 
   someInstance.size = function(){
+    return count;
   };
 
   return someInstance;
