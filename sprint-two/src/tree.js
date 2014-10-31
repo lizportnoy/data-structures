b6bd833856ff4babc8e6d7e80091239782b94ba6
lizportnoy <lizport10@gmail.com>
2014-10-31 16:40:30 -0700

    tree progress

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -6,26 +6,14 @@ var makeTree = function(value){
   return newTree;
 };
 
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-  // check if an array exists at children
   if (!this.children) {
     this.children = [];
   }
   var child = makeTree(value);
   this.children.push(child);
-  // add value to array in format of object {value: val, children : undefined}
-
-  function recursiveSubroutine(child) {
-
-  }
-
-  recursiveSubroutine(node);
-
 };
 
 treeMethods.contains = function(target){
