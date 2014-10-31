5bd596fee3602412434a3159d6704565bc49d128
lizportnoy <lizport10@gmail.com>
2014-10-31 13:24:07 -0700

    working through hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,16 +3,61 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value){
+  var i = getIndexBelowMaxForKey(key, this._limit);
+  if(!this._storage.get(i)){
+    this._storage.set(i, []);
+  }
+  var bucket = this._storage.get(i);
+  var temp = {};
+  temp[key] = value;
+  bucket.push(temp);
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  var result;
+  var bucket = this._storage.get(i);
+
+  this._storage.each(function(bucketArray){
+    if(bucketArray === bucket){
+      return _.each(bucketArray, function(obj){
+        return _.each(obj, function(val, key){
+          if (key === k){
+            result = val;
+          }
+        });
+      });
+    }
+  });
+
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var target;
+  this._storage.each(function(bucketArray) {
+    if(bucketArray === bucket){
+      _.each(bucketArray, function(obj, index, bucketArray) {
+
+        _.each(obj, function(val, key, obj){
+          if (key === k) {
+            var target = obj;
+          }
+        });
+
+        if(_.indexOf(bucketArray, target) === index) {
+          bucketArray.splice(index, 0);
+        }
+
+      });
+
+    }
+  });
 
 };
 
